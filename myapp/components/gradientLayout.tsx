9428bc3dcf3876f016ff04a8e2fb0c9fbdc0294a
ilmfeemster <ilmfeemster@gmail.com>
2022-05-02 07:08:09 -0700

    add server side artist props and homepage render

--- myapp/components/gradientLayout.tsx
+++ myapp/components/gradientLayout.tsx
@@ -36,6 +36,7 @@ return (
                 </Text>
             </Box>
         </Flex>
+        <Box paddingY="50px">{children}</Box>
     </Box>
 )
 }
--- myapp/pages/index.tsx
+++ myapp/pages/index.tsx
@@ -1,20 +1,51 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Box, Text, Flex } from "@chakra-ui/layout"
+import { Image } from "@chakra-ui/react"
 import GradientLayout from "../components/gradientLayout"
-import styles from '../styles/Home.module.css'
+import prisma from '../lib/prisma'
 
-const Home = () => {
-  return (
-  <GradientLayout 
-  roundImage="true"
-  color="red" 
-  subtitle="profile" 
-  title="Immanuel Matthews" 
-  description="15 public playlists"
-  image="/spoofify_profile_pic.png">
-    <div>home page</div>
-  </GradientLayout>
-  )
+const Home = ({artists}) => {
+	return (
+	<GradientLayout 
+	roundImage="true"
+	color="cyan" 
+	subtitle="profile" 
+	title="Immanuel Matthews" 
+	description="15 public playlists"
+	image="/spoofify_profile_pic.png">
+		<div>home page</div>
+
+	<Box color="white" paddingX="40px">
+		<Box marginBottom="40px">
+			<Text fontSize="2xl" fontWeight="bold">Top artists this month</Text>
+			<Text fontSize="medium">only visible to you</Text>
+		</Box>
+		<Flex>
+			{artists.map((artist) => (
+				<Box paddingX="10px" width="20%">
+				<Box bg="gray.900" borderRadius="4px" padding ="15px" width="100%">
+					<Image 
+					src="/albumplaceholder.png" 
+					borderRadius="100%"
+					/>
+					<Box marginTop="20px">
+						<Text fontSize="Large">{artist.name}</Text>
+						<Text fontSize="x-small">Artist</Text>
+					</Box>
+				</Box>
+				</Box>
+			))}
+		</Flex>
+	</Box>
+	</GradientLayout>
+	)
 }
+
+export const getServerSideProps = async () => {
+	const artists = await prisma.artist.findMany({})
+  
+	return {
+	  props: { artists },
+	}
+    }
 
 export default Home
